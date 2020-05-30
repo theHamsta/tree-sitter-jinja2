d9ae9752526aef84dda5be7e8b8a66ca138f98b8
Stephan Seitz <stephan.seitz@fau.de>
2020-05-30 14:35:40 +0200

    Initial commit

---
+++ grammar.js
@@ -0,0 +1,61 @@
+/*
+ * grammar.js
+ * Copyright (C) 2020 Stephan Seitz <stephan.seitz@fau.de>
+ *
+ * Distributed under terms of the GPLv3 license.
+ */
+
+
+
+    //{% ... %} for Statements
+
+    //{{ ... }} for Expressions to print to the template output
+
+    //{# ... #} for Comments not included in the template output
+
+    //#  ... ## for Line Statements
+
+//valid:
+
+//{%- if foo -%}...{% endif %}
+
+//invalid:
+
+//{% - if foo - %}...{% endif %}
+
+whitespace_control = /-?\+?/
+
+module.exports = grammar ({
+  name: 'Jinja2',
+
+  rules: {
+    source_file: $ => repeat($._block),
+
+    _block: $ => choice($.statement, $.expression, $.line_statement, $.comment, $.text),
+
+    for_statement: $ => seq($.startfor, repeat($._block), $.endfor),
+    startfor: $ => seq('{%', whitespace_control, /\s*for\s*/, $.jinja_stuff, whitespace_control, '%}'),
+    endfor: $ => seq('{%', whitespace_control, /\s*endfor\s*/, whitespace_control, '%}'),
+
+    if_statement: $ => seq($.startif, repeat($._block), $.endif),
+    startif: $ => seq('{%', whitespace_control, /\s*if\s*/, $.jinja_stuff,  whitespace_control,'%}'),
+    endif: $ => seq('{%', whitespace_control, /\s*endif\s*/, whitespace_control, '%}'),
+
+    raw_statement: $ => seq($.startraw, $.rawtext, $.endraw),
+    startraw: $ => seq('{%', whitespace_control, /\s*raw\s*/,  whitespace_control,'%}'),
+    endraw: $ => seq('{%', whitespace_control, /\s*endraw\s*/, whitespace_control,'%}'),
+
+    expression: $ => seq('{{', whitespace_control, $.jinja_stuff, whitespace_control,'}}'),
+    statement: $ => choice($.for_statement, $.if_statement, $.raw_statement),
+    line_statement: $ => prec(2, seq('^#', $.jinja_stuff, optional('##'))),
+    comment: $ => seq('{#', $.rawtext, '#}'),
+
+    _text: $ =>  choice(/[^{}%#]+/, '{', '}', '#', '%'),
+    _rawtext: $ =>  choice($._text, '{{', '}}', '{%', '%}', '{#', '#}'),
+
+    jinja_stuff: $ =>  prec.left(1, repeat1($._text)),
+    text: $ =>  prec.left(1, repeat1($._text)),
+    rawtext: $ => prec.left(1, repeat1($._rawtext)), 
+ 
+  }
+});

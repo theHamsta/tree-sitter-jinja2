fb6a0677a779c2a9b13b9086eeaee417764a0303
Stephan Seitz <stephan.seitz@fau.de>
2020-05-30 20:46:15 +0200

    Make whitespace_control a named node

--- grammar.js
+++ grammar.js
@@ -23,7 +23,7 @@
 
 //{% - if foo - %}...{% endif %}
 
-whitespace_control = /(-|\+)?/
+maybe_whitespace_control = $ => optional($.whitespace_control)
 block = $ => repeat($._block_statement)
 
 module.exports = grammar ({
@@ -38,66 +38,68 @@ module.exports = grammar ({
 
   rules: {
     source_file: $ => repeat($._block_statement),
+    whitespace_control: $ => /[-|\+]/,
     
     _block_statement: $ => choice($.jinja_scope, $.expression, $.line_statement, $.comment, $.text),
+    white_space_control: $ => choice($.jinja_scope, $.expression, $.line_statement, $.comment, $.text),
 
     for_statement: $ => seq($.startfor, block($), repeat(seq($.elif_statement, block($))), optional(seq($.else_statement, block($))), $.endfor),
-    startfor: $ => seq('{%', whitespace_control, /\s*for\s*/, $.jinja_stuff,  whitespace_control, '%}'),
-    endfor: $ => seq('{%', whitespace_control, /\s*endfor\s*/, whitespace_control, '%}'),
+    startfor: $ => seq('{%', maybe_whitespace_control($), /\s*for\s*/, $.jinja_stuff,  maybe_whitespace_control($), '%}'),
+    endfor: $ => seq('{%', maybe_whitespace_control($), /\s*endfor\s*/, maybe_whitespace_control($), '%}'),
 
     if_statement: $ => seq($.startif, block($), repeat(seq($.elif_statement, block($))), optional(seq($.else_statement, block($))), $.endif),
-    startif: $ => seq('{%', whitespace_control, /\s*if\s*/, $.jinja_stuff,  whitespace_control,'%}'),
-    endif: $ => seq('{%', whitespace_control, /\s*endif\s*/, whitespace_control, '%}'),
-    else_statement: $ => seq('{%', whitespace_control, /\s*else\s*/, whitespace_control,'%}'),
-    elif_statement: $ => seq('{%', whitespace_control, /\s*elif\s*/, $.jinja_stuff, whitespace_control,'%}'),
+    startif: $ => seq('{%', maybe_whitespace_control($), /\s*if\s*/, $.jinja_stuff,  maybe_whitespace_control($),'%}'),
+    endif: $ => seq('{%', maybe_whitespace_control($), /\s*endif\s*/, maybe_whitespace_control($), '%}'),
+    else_statement: $ => seq('{%', maybe_whitespace_control($), /\s*else\s*/, maybe_whitespace_control($),'%}'),
+    elif_statement: $ => seq('{%', maybe_whitespace_control($), /\s*elif\s*/, $.jinja_stuff, maybe_whitespace_control($),'%}'),
 
     raw_statement: $ => seq($.startraw, $.rawtext, $.endraw),
-    startraw: $ => seq('{%', whitespace_control, /\s*raw\s*/,  whitespace_control,'%}'),
-    endraw: $ => seq('{%', whitespace_control, /\s*endraw\s*/, whitespace_control,'%}'),
+    startraw: $ => seq('{%', maybe_whitespace_control($), /\s*raw\s*/,  maybe_whitespace_control($),'%}'),
+    endraw: $ => seq('{%', maybe_whitespace_control($), /\s*endraw\s*/, maybe_whitespace_control($),'%}'),
 
     macro_statement: $ => seq($.startmacro, block($), $.endmacro),
-    startmacro: $ => seq('{%', whitespace_control, /\s*macro\s*/, $.jinja_stuff,  whitespace_control,'%}'),
-    endmacro: $ => seq('{%', whitespace_control, /\s*endmacro\s*/, whitespace_control,'%}'),
+    startmacro: $ => seq('{%', maybe_whitespace_control($), /\s*macro\s*/, $.jinja_stuff,  maybe_whitespace_control($),'%}'),
+    endmacro: $ => seq('{%', maybe_whitespace_control($), /\s*endmacro\s*/, maybe_whitespace_control($),'%}'),
 
-    extends_statement: $ => seq('{%', whitespace_control, /\s*extends\s*/,  $.jinja_stuff,  whitespace_control,'%}'),
+    extends_statement: $ => seq('{%', maybe_whitespace_control($), /\s*extends\s*/,  $.jinja_stuff,  maybe_whitespace_control($),'%}'),
 
     block_statement: $ => seq($.startblock, block($), $.endblock),
-    startblock: $ => seq('{%', whitespace_control, /\s*block\s*/, $.jinja_stuff,  whitespace_control,'%}'),
-    endblock: $ => seq('{%', whitespace_control, /\s*endblock\s*/, optional($.jinja_stuff), whitespace_control,'%}'),
+    startblock: $ => seq('{%', maybe_whitespace_control($), /\s*block\s*/, $.jinja_stuff,  maybe_whitespace_control($),'%}'),
+    endblock: $ => seq('{%', maybe_whitespace_control($), /\s*endblock\s*/, optional($.jinja_stuff), maybe_whitespace_control($),'%}'),
 
     call_statement: $ => seq($.startcall, block($), $.endcall),
-    startcall: $ => seq('{%', whitespace_control, /\s*call\s*/, $.jinja_stuff,  whitespace_control,'%}'),
-    endcall: $ => seq('{%', whitespace_control, /\s*endcall\s*/, whitespace_control,'%}'),
+    startcall: $ => seq('{%', maybe_whitespace_control($), /\s*call\s*/, $.jinja_stuff,  maybe_whitespace_control($),'%}'),
+    endcall: $ => seq('{%', maybe_whitespace_control($), /\s*endcall\s*/, maybe_whitespace_control($),'%}'),
 
     filter_statement: $ => seq($.startfilter, block($), $.endfilter),
-    startfilter: $ => seq('{%', whitespace_control, /\s*filter\s*/, $.jinja_stuff,  whitespace_control,'%}'),
-    endfilter: $ => seq('{%', whitespace_control, /\s*endfilter\s*/, whitespace_control,'%}'),
+    startfilter: $ => seq('{%', maybe_whitespace_control($), /\s*filter\s*/, $.jinja_stuff,  maybe_whitespace_control($),'%}'),
+    endfilter: $ => seq('{%', maybe_whitespace_control($), /\s*endfilter\s*/, maybe_whitespace_control($),'%}'),
 
     block_set_statement: $ =>  seq($.set_statement, block($), $.endset),
-    set_statement: $ => seq('{%', whitespace_control, /\s*set\s*/, $.jinja_stuff,  whitespace_control,'%}'),
-    endset: $ => seq('{%', whitespace_control, /\s*endset\s*/, whitespace_control,'%}'),
+    set_statement: $ => seq('{%', maybe_whitespace_control($), /\s*set\s*/, $.jinja_stuff,  maybe_whitespace_control($),'%}'),
+    endset: $ => seq('{%', maybe_whitespace_control($), /\s*endset\s*/, maybe_whitespace_control($),'%}'),
 
-    include_statement: $ => seq('{%', whitespace_control, /\s*include\s*/, $.jinja_stuff, whitespace_control,'%}'),
-    import_statement: $ => seq('{%', whitespace_control, /\s*import\s*/, $.jinja_stuff, whitespace_control,'%}'),
-    from_statement: $ => seq('{%', whitespace_control, /\s*from\s*/, $.jinja_stuff, whitespace_control,'%}'),
+    include_statement: $ => seq('{%', maybe_whitespace_control($), /\s*include\s*/, $.jinja_stuff, maybe_whitespace_control($),'%}'),
+    import_statement: $ => seq('{%', maybe_whitespace_control($), /\s*import\s*/, $.jinja_stuff, maybe_whitespace_control($),'%}'),
+    from_statement: $ => seq('{%', maybe_whitespace_control($), /\s*from\s*/, $.jinja_stuff, maybe_whitespace_control($),'%}'),
 
     autoescape_statement: $ => seq($.startautoescape, block($), $.endautoescape),
-    startautoescape: $ => seq('{%', whitespace_control, /\s*autoescape\s*/, $.jinja_stuff,  whitespace_control,'%}'),
-    endautoescape: $ => seq('{%', whitespace_control, /\s*endautoescape\s*/, whitespace_control,'%}'),
+    startautoescape: $ => seq('{%', maybe_whitespace_control($), /\s*autoescape\s*/, $.jinja_stuff,  maybe_whitespace_control($),'%}'),
+    endautoescape: $ => seq('{%', maybe_whitespace_control($), /\s*endautoescape\s*/, maybe_whitespace_control($),'%}'),
 
     trans_statement: $ => seq($.starttrans, block($), optional(seq($.pluralize, block($))), $.endtrans),
-    starttrans: $ => seq('{%', whitespace_control, /\s*trans\s*/, $.jinja_stuff,  whitespace_control, '%}'),
-    endtrans: $ => seq('{%', whitespace_control, /\s*endtrans\s*/, whitespace_control, '%}'),
-    pluralize: $ => seq('{%', whitespace_control, /\s*pluralize\s*/, whitespace_control,'%}'),
+    starttrans: $ => seq('{%', maybe_whitespace_control($), /\s*trans\s*/, $.jinja_stuff,  maybe_whitespace_control($), '%}'),
+    endtrans: $ => seq('{%', maybe_whitespace_control($), /\s*endtrans\s*/, maybe_whitespace_control($), '%}'),
+    pluralize: $ => seq('{%', maybe_whitespace_control($), /\s*pluralize\s*/, maybe_whitespace_control($),'%}'),
 
     with_statement: $ => seq($.startwith, block($), $.endwith),
-    startwith: $ => seq('{%', whitespace_control, /\s*with\s*/, $.jinja_stuff,  whitespace_control,'%}'),
-    endwith: $ => seq('{%', whitespace_control, /\s*endwith\s*/, whitespace_control,'%}'),
+    startwith: $ => seq('{%', maybe_whitespace_control($), /\s*with\s*/, $.jinja_stuff,  maybe_whitespace_control($),'%}'),
+    endwith: $ => seq('{%', maybe_whitespace_control($), /\s*endwith\s*/, maybe_whitespace_control($),'%}'),
 
-    debug_statement: $ => seq('{%', whitespace_control, /\s*debug\s*/,  whitespace_control,'%}'),
-    do_statement: $ => seq('{%', whitespace_control, /\s*do\s*/, $.jinja_stuff, whitespace_control,'%}'),
+    debug_statement: $ => seq('{%', maybe_whitespace_control($), /\s*debug\s*/,  maybe_whitespace_control($),'%}'),
+    do_statement: $ => seq('{%', maybe_whitespace_control($), /\s*do\s*/, $.jinja_stuff, maybe_whitespace_control($),'%}'),
 
-    expression: $ => seq('{{', whitespace_control, $.jinja_stuff, whitespace_control,'}}'),
+    expression: $ => seq('{{', maybe_whitespace_control($), $.jinja_stuff, maybe_whitespace_control($),'}}'),
     jinja_scope: $ => choice($.for_statement,
                              $.if_statement,
                              $.raw_statement,
